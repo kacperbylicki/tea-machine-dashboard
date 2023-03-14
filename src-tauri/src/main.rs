2cfbdc447c5942b578139b6fef238cd19f540458
Kacper Bylicki <kacper@bylicki.dev>
2023-03-14 19:56:16 +0100

    feat: add basic rust function and invoke it in vue component

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,12 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![hello_rust])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn hello_rust() -> &'static str {
+  return "Hello from Rust!"
 }
--- src/views/TeaDetails.vue
+++ src/views/TeaDetails.vue
@@ -1,11 +1,13 @@
 <template>
     <h2>{{ tea?.name }} - Tea Details</h2>
+    <button @click="invokeHelloRust()">Hello Rust</button>
     <button @click="redirect()">Back</button>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import { invoke } from "@tauri-apps/api/tauri";
 import teaJson from '@/assets/tea.json'
 
 const route = useRoute();
@@ -13,6 +15,15 @@ const router = useRouter();
 
 const redirect = () => {
   router.push("/");
+};
+
+const invokeHelloRust = async () => {
+  try {
+    const response = await invoke("hello_rust")
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getTeaFromSlug = (slug: string | string[]) => {
